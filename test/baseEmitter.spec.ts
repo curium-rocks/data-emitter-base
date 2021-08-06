d3b445514f28f14260aaaa56b8348873a943871a
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-08-06 15:38:03 -0500

    add test stubs

---
+++ test/baseEmitter.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it} from 'mocha';
+import { should } from 'chai';
+
+describe( 'BaseEmitter', function() {
+    describe( 'serializeState()', function() {
+        it( 'Should allow plain text serialization', function() {
+            should().fail("Not implemented");
+        });
+        it( 'Should allow aes gcm serialization', function() {
+            should().fail("Not implemented");
+        });
+        it( 'should be compatible with recreateEmitter in provider', function() {
+            should().fail("Not implemented");
+        });
+    });
+});
---
+++ test/provider.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it} from 'mocha';
+import { should } from 'chai';
+
+describe( 'Provider', function() {
+    describe( 'registerEmitterFactory()', function() {
+        it( 'Should result in factory calls when using that type', function() {
+            should().fail();
+        });
+    });
+    describe( 'registerChroniclerFactory()', function() {
+        it( 'Should result in factory calls when using that type', function() {
+            should().fail();
+        });
+    });
+    describe( 'recreateEmitter()', function() {
+        it( 'Should create a previous emitter with the same state', function() {
+            should().fail();
+        });
+    });
+    describe( 'buildEmitter()', function() {
+        it( 'Should build an emitter to expected specifications', function() {
+            should().fail();
+        });
+    });
+    describe( 'buildChronicler()', function() {
+        it( 'Should build an chronicler to expected specifications', function() {
+            should().fail();
+        });
+    });
+});
