3bb698a880cf30d33197eb174a86c4561ab6d895
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-08-29 07:44:27 -0500

    update exports

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,6 @@
 export {IDataEmitter, ICompoundDataEmitter, ICommand, IDataEvent, IExecutionResult, ISettings, ITraceableAction, IEmitterFactory, 
     IDisposableAsync, IDisposable, IClassifier, IDataEventListener, IDataEventListenerFunc, IStatusChangeListener, IStatusChangeListenerFunc, IStatusEvent,
-    IEmitterProvider, IChroniclerFactory, IChroniclerProvider, IEmitterDescription, IChroniclerDescription, IFormatSettings, isJsonSerializable, isDisposable, isDisposableAsync, hasMethod} from './dataEmitter';
+    IEmitterProvider, IChroniclerFactory, IChroniclerProvider, IEmitterDescription, IChroniclerDescription, IFormatSettings, isJsonSerializable} from './dataEmitter';
 export {BaseEmitter, BaseStatusEvent, BaseDataEvent} from './baseEmitter';
 export {PollingEmitter, DeltaPollingEmitter} from './pollingEmitter';
 export {LoggerFacade, LogLevel} from './loggerFacade';
@@ -8,4 +8,4 @@ export {IChronicler, IFileChronicler, IRotatingFileChronicler, IJsonSerializable
 export {IService, IChroniclerMaestro, IEmitterMaestro, IMaestro, isService} from './maestro';
 export {ProviderSingleton} from './provider';
 export {BaseFactory, BaseEmitterFactory, BaseChroniclerFactory } from './factory';
-export {ISerializableState} from './common';
+export {ISerializableState, isDisposableAsync, isDisposable, hasMethod} from './common';
